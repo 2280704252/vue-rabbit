64ce66ec85b7b24eabd9d7e7b744084d893458cf
2280704252 <2280704252@qq.com>
2024-04-17 22:44:09 +0800

    引入scss文件

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
 
-
+<!--一级出口路由-->
 <!--路由出口，路由匹配到的组件将在这里渲染-->
   <router-view></router-view>
+
+  <div class="test">Test scss</div>
 </template>
+
+
+<style scoped lang="scss">
+.test{
+
+color:$sucColor ;
+
+}
+
+
+
+</style>
