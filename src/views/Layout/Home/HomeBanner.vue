9be1abeadf0ce6af0b847b85434643b054228dee
2280704252 <2280704252@qq.com>
2024-04-23 14:39:09 +0800

    实现轮播图

--- src/views/Layout/Home/HomeBanner.vue
+++ src/views/Layout/Home/HomeBanner.vue
@@ -1,7 +1,57 @@
 <script setup>
 
+
+import {getBannerAPI} from '@/apis/getBanner'
+import { onMounted,ref } from 'vue'
+
+const bannerList =ref([])
+
+
+const getBanner = async()=>{
+
+    const res = await getBannerAPI()
+    console.log(res)
+
+    bannerList.value=res.result
+
+}
+
+onMounted(()=>{
+
+    getBanner()
+})
+
+
+
+
 </script>
 
+
+
 <template>
-    轮播图
+  <div class="home-banner">
+    <el-carousel height="500px">
+      <el-carousel-item v-for="item in bannerList" :key="item.id">
+        <img :src="item.imgUrl" alt="">
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
+
+
+
+<style scoped lang='scss'>
+.home-banner {
+  width: 1000px;
+  height: 500px;
+  position: relative;
+  left: 20%;
+  top: -500px;
+  z-index: 98;
+
+  img {
+    width: 99%;
+    height: 500px;
+  }
+}
+</style>
